4e6d1b448fa944f6767351e5722d1aae1806d772
zatzk <zatzk@outlook.com>
2023-06-11 12:08:15 -0300

    fix: change metadata to next/head 🍔

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
+import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Pedido de namoro 🥺',
-  description: 'aceitas?',
-}
 
 export default function RootLayout({
   children,
@@ -15,6 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Pedido de namoro 🥺</title>
+        <meta name="description" content="aceitas?" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
