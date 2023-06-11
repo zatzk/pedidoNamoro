b3e462bf763de82dc726723e5820f07e551362f2
zatzk <zatzk@outlook.com>
2023-06-11 12:25:09 -0300

    fix: change metadata to next/head 🍔

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Pedido de namoro 🥺',
-  description: 'aceitas?',
-}
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <>
+      <Head>
+        <title>Pedido de namoro 🥺</title>
+        <meta name="description" content="aceitas?" />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
+  );
 }
